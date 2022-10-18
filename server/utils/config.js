63ec74ee1125678820f49867553e3735543cf841
nas11ai <kairupannasai@gmail.com>
2022-10-18 14:46:21 +0800

    feat: enable admin to pin an image from a folder to pinata

--- server/utils/config.js
+++ server/utils/config.js
@@ -4,4 +4,6 @@ module.exports = {
   DATABASE_URL: process.env.DATABASE_URL,
   PORT: process.env.PORT || 3001,
   SECRET: process.env.SECRET,
+  PINATA_API_KEY: process.env.PINATA_API_KEY,
+  PINATA_API_SECRET: process.env.PINATA_API_SECRET,
 };
